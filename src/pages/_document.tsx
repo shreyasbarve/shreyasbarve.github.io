2e8e0b24db847341c74dd7d84be11cfd4c79baae
shreyasbarve <shreyasbarve13@gmail.com>
2023-08-16 03:51:22 +0530

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/static/favicon.jpg"
+          type="image/x-icon"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
